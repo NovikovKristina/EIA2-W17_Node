23ca513e3f6b99187708ce10fb71cc54cde6ec2f
Kristina Novikov <novikov-kristina@web.de>
2018-01-22 22:40:47 +0100

    push changes

--- ServerTest.ts
+++ ServerTest.ts
@@ -37,11 +37,11 @@ namespace ServerTest {
         _response.setHeader("Access-Control-Allow-Origin", "*");
         
         // Response-Body
-        _response.write("Ich höre Stimmen!!<br>");
-        _response.write("Port: " + port + "<br>");
-        _response.write("Method: " + _request.method + "<br>");
-        _response.write("Url: " + _request.url + "<br>");
-        _response.write("Headers: " + _request.headers + "<br>");
+        _response.write("Ihre Bestellung wurde entgegengenommen und befindet sich derzeit in Bearbeitung<br>");
+//        _response.write("Port: " + port + "<br>");
+//        _response.write("Method: " + _request.method + "<br>");
+//        _response.write("Url: " + _request.url + "<br>");
+//        _response.write("Headers: " + _request.headers + "<br>");
 
         // ?
         let query: AssocStringString = Url.parse(_request.url, true).query;
